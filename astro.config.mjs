4bfdac7bfe17161e1233fa0560b00bade99d8c20
beiqi0088 <240200103@qq.com>
2024-05-29 17:57:59 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://jidi0088.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
